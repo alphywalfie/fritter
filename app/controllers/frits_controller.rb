e13706bbefe58f5d68a3550dc66ec29de9a91c07
Azi <azidelapaz@gmail.com>
2014-05-23 14:23:08 +0800

    more edits

--- app/controllers/frits_controller.rb
+++ app/controllers/frits_controller.rb
@@ -6,7 +6,9 @@ class FritsController < ApplicationController
 	def new
 		@prompt = Prompt.Sample(1)
 		@limit = Limit.Sample(1)
-		@frit = Frit.new
+		frit = Frit.new
+#		current_prompt = @prompt
+#		current_limit = @limit
 	end
 
 	def create
@@ -14,8 +16,8 @@ class FritsController < ApplicationController
 		@frit.content = params[:frit][:content]
 		@frit.posted_on = DateTime.now
 		@frit.user = current_user
-		#@frit.prompt =
-		#@frit.limit =
+		@frit.prompt = current_prompt
+		@frit.limit = current_limit
 		@frit.save!
 	end
 end
