96654bc837636994c0b6f283c16f982eb71e83d7
Azi <azidelapaz@gmail.com>
2014-05-23 16:43:20 +0800

    yooo

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   resources :limits
   
   root 'pages#index'
+#  devise_for :user do
+#   	get '/login', :to => 'devise/sessions#new' # Add a custom sign in route for user sign in
+#   	get '/logout', :to => 'devise/sessions#destroy' # Add a custom sing out route for user sign out
+#   	get '/register', :to => 'devise/registrations#new' # Add a Custom Route for Registrations
+#  end
 #get '/admin' => 'pages#admin_index'
 #get '/admin/create_prompt' => 'pages#admin_create_prompt'
 #get '/admin/edit_prompt' => 'pages#admin_edit_prompt/:id'
