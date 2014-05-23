ccbcd83c95110a88c4aba252cf660eedb9356855
heutee <hannah_332004@yahoo.com>
2014-05-23 20:49:10 +0800

    fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,33 +1,6 @@
 class PagesController < ApplicationController
-
 	def index
 		@latest_frits = Frit.last(10)
 	end
-
-#	def admin_index
-#		@password = "potatoes"
-#	end
-
-#	def admin_create_prompt
-#	end
-
-#	def admin_edit_prompt
-#		@prompt = Prompt.find(params[:id])
-#	end
-
-#	def admin_delete_prompt
-#		@prompt = Prompt.find(params[:id])
-#	end
-
-#	def admin_create_limit
-#	end
-
-#	def admin_edit_limit
-#		@limit = Limit.find(params[:id])
-#	end
-
-#	def admin_delete_limit
-#		@limit = Limit.find(params[:id])
-#	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  resources :fritters
+  resources :frits
   resources :users
   resources :prompts
   resources :limits
