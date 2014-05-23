55d5c921a98a0b128c0b91243a3b57fd492972ac
Azi <azidelapaz@gmail.com>
2014-05-23 20:53:48 +0800

    fixed new frit page

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,6 +10,8 @@ class Ability
             can :manage, :all        
         end
     end
+
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
